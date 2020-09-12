bd0825b704f77274cc7f631d9a07cf008db6ab10
quan005 <ralphealfreeman@gmail.com>
2020-09-12 15:51:29 -0700

    Mvp

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -4,6 +4,29 @@ def intersection(arrays):
     """
     # Your code here
 
+    # First we add our Hash Table
+    hash_table = {}
+
+    # Add list to return intersections
+    result = []
+
+    # Iterate through each list within the arrays list
+    for array in arrays:
+        # Iterate through each integer within each given list
+        for integer in array:
+            # Check if the Hash Table have the current integer
+            if integer in hash_table:
+                # If so, check if it was added to the result list or not
+                if integer in result:
+                    pass
+                else:
+                    # If not add the integer to the result list
+                    result.append(integer)
+            else:
+                # if not add the integer to the Hash Table with the 
+                # value of 1 to represent the number of times that integer is shown
+                hash_table[integer] = 1
+
     return result
 
 
